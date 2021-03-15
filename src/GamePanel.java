cfe546dcdb4b9f3da592a6242efa7a6a971c9039
= Assiri Karim <=mirak94@hotmail.com>
2021-03-15 19:35:17 +0100

    create classes annd some methods

---
+++ src/GamePanel.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel  implements ActionListener {
+
+    GamePanel (){
+
+    }
+    public void startGame(){
+
+    }
+    public void move(){}
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw(Graphics g){
+        
+    }
+    public void checkApple(){
+        
+    }
+    public void checkCollicsion(Graphics g){
+
+    }
+    public void gameOver(){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
+}
